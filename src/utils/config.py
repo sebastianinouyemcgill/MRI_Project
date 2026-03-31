88edd3f43b6c6d8e8b62c558d534449b6aa5e278
Sebastian Inouye <sebastian.inouye@mail.mcgill.ca>
2026-03-31 17:22:27 -0400

    config

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,9 +2,10 @@
 import os
 
 # Paths
-DATA_ROOT = "../../data/processed"       # for preprocessed tensors
+DATA_ROOT = "/Volumes/SSD 2/Projects/MRI Project/Processed Data" # "../../data/processed"
 SPLIT_ROOT = "../../splits"             # for train/test/val txts
 JSON_ROOT = "../../data/json"           # for volumes.json / labels.json
+CHECKPOINT_ROOT = "../../checkpoint/fulltest1"   # for saving model checkpoints
 
 # Sliding window / sequences
 SEQ_LEN = 2                              # number of timepoints per sequence
