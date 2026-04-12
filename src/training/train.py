24fbb00c2dac1f594f804fbdefbd21c2c6d22e99
Sebastian Inouye <sebastian.inouye@mail.mcgill.ca>
2026-04-12 14:29:20 -0400

    train cuda

--- src/training/train.py
+++ src/training/train.py
@@ -47,7 +47,7 @@ from training.losses import BinaryClassificationLoss
 criterion = BinaryClassificationLoss(
     pos_weight=2978 / 1512,   # from your Counter output
     label_smoothing=0.1        # optional, start with 0.0 if you want baseline first
-)
+).to("cuda" if torch.cuda.is_available() else "cpu")
 
 def train():
     # Device
