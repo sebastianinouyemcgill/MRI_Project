28317e1f245eee7befd79a0a73e712630ca6582a
Sebastian Inouye <sebastian.inouye@mail.mcgill.ca>
2026-04-14 13:59:45 -0400

    config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,29 +1,25 @@
-# config.py
 import os
 
-LOCAL_ROOT    = '/content/MRI_Project'
+class Config:
+    def __init__(self):
+        self.LOCAL_ROOT   = "/content/drive/MyDrive/MRI_PROJECT"
+        self.DATA_ROOT = os.path.join(self.LOCAL_ROOT, "data/processed/Processed Post")
+        self.SPLIT_ROOT = os.path.join(self.LOCAL_ROOT, "splits")
+        self.JSON_ROOT = os.path.join(self.LOCAL_ROOT, "data/json")
+
+        self.SEQ_LEN = 3
+        self.GROWTH_THRESHOLD = 0.2
+        self.RANDOM_SEED = 42
+        self.BATCH_SIZE = 4
+        self.FEATURE_DIM = 256
+        self.MODALITY = "POST"
+        self.NUM_EPOCHS = 20
+        self.LR = 1e-4
+
+cfg = Config()
 
 # Paths
-DATA_ROOT =  os.path.join(LOCAL_ROOT, 'data/processed') # "../../data/processed"  # "/Volumes/SSD 2/Projects/MRI Project/Processed Data"
-SPLIT_ROOT = os.path.join(LOCAL_ROOT, 'splits') # "../../splits/mini"             # for train/test/val txts
-JSON_ROOT = os.path.join(LOCAL_ROOT, 'data/json') # "../../data/json/mini"           # for volumes.json / labels.json
-CHECKPOINT_ROOT = os.path.join(LOCAL_ROOT, 'checkpoints') # "../../checkpoint/minitest"   # for saving model checkpoints
-
-# Sliding window / sequences
-SEQ_LEN = 2                              # number of timepoints per sequence
-
-# Labels
-GROWTH_THRESHOLD = 0.2
-
-# Randomness
-RANDOM_SEED = 42
-
-# CNN/LSTM
-BATCH_SIZE = 4
-FEATURE_DIM = 256  # placeholder for CNN output size
-
-# Misc
-MODALITY = "POST"  # only using POST for now
-
-NUM_EPOCHS = 10
-LR = 1e-4
+# DATA_ROOT =  os.path.join(LOCAL_ROOT, 'data/processed') # "../../data/processed"  # "/Volumes/SSD 2/Projects/MRI Project/Processed Data"
+# SPLIT_ROOT = os.path.join(LOCAL_ROOT, 'splits') # "../../splits/mini"             # for train/test/val txts
+# JSON_ROOT = os.path.join(LOCAL_ROOT, 'data/json') # "../../data/json/mini"           # for volumes.json / labels.json
+# CHECKPOINT_ROOT = os.path.join(LOCAL_ROOT, 'checkpoints') # "../../checkpoint/minitest"   # for saving model checkpoints
